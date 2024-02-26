7b0fd1f72889958c94035f2aab38ad6a6d171bb7
Roman Batrakov <Kyct69@yandex.ru>
2024-02-26 22:03:36 +0300

    Complete project NumbersFibonachi

--- NumbersFibonachi/src/Main.java
+++ NumbersFibonachi/src/Main.java
@@ -1,5 +1,20 @@
+/**
+ * Программы выводит пользователю числа Фибоначи от 1
+ * Каждый его элемент формируется как сумма двух предыдущих элементов
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Инициализация переменных
+        int n0 = 1, n1 = 1, n2 = 0;
+        // Выводим первые элементы
+        System.out.print(n0 + " ");
+        System.out.print(n1 + " ");
+        // В цикле суммируем элементы
+        for(int i = 1; i < 18; i++){
+            n2 = n0 + n1;
+            System.out.print(n2 + " ");
+            n0 = n1;
+            n1 = n2;
+        }
     }
 }
